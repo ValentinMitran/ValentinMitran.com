2d54d4384c48e39ef9205cfdf8654f3f80ead966
Valentin Mitran <ToolsVM@pm.me>
2020-12-02 18:41:45 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
               linkedin
             </a>
             <a
-              href="https://drive.google.com/file/d/1BC63TwSXivTFdvMOA_C5QK_O6dwYr3Jy/view?usp=sharing"
+              href="https://drive.google.com/file/d/1xza7rfhKXHA_FUIXgKH8400hF_UkfmzY/view?usp=sharing"
               target="_blank"
               rel="noreferrer"
             >
